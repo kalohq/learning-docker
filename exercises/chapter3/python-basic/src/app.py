067b34181494071d6468be448a13799280e29e6e
Yannis Panousis <yannis@lystable.com>
2017-02-16 15:51:08 +0000

    Working example of Chapter3 exercise

--- exercises/chapter3/python-basic/src/app.py
+++ exercises/chapter3/python-basic/src/app.py
@@ -1,11 +1,46 @@
 #!/usr/bin/python
-from flask import Flask
 
-app = Flask(__name__)
+import random
+import requests
+
+from flask import Flask, redirect, render_template
+
+app = Flask(__name__, static_url_path='')
+
+# API_KEY = os.environ['GIPHY_API_KEY']
+API_KEY = 'dc6zaTOxFJmzC'
+
 
 @app.route('/')
 def entry_point():
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.route('/query/<path:query>')
+def what(query):
+    # Query Giphy API if there is a query
+    response = requests.get(
+        'http://api.giphy.com/v1/gifs/search?q={}&api_key={}&limit=10'.format(
+            query, API_KEY
+        )
+    )
+    giphy_gifs = response.json()['data']
+
+    # Show the first GIF
+    if len(giphy_gifs) > 0:
+        return redirect(
+            giphy_gifs[
+                random.randint(0, 100) % len(giphy_gifs)
+            ]['images']['original']['url'],
+            code=302
+        )
+    # Show a failure message
+    else:
+        return render_template(
+            'nogif.html', title='Awww, no GIF for {}'.format(query)
+        )
+
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    # app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
